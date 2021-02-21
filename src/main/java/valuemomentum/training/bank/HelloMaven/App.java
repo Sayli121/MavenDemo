8e2338740a3a11015d482305b64d477867929b98
sayli <spusegaonkar4@gmail.com>
2021-02-21 10:32:06 +0530

    final commit 1.0

---
+++ src/main/java/valuemomentum/training/bank/HelloMaven/App.java
@@ -0,0 +1,12 @@
+package valuemomentum.training.bank.HelloMaven;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "***Banking Application***" );
+        		 Customer c1=new Customer(111,"Raj",5000);
+        		 
+        		 c1.display();
+    }
+}
